feda4b8c5a8bf7bbcc37116c2a61870b8182e8c1
Gregory Brutsche <gregory@mail.brutsches.local>
2021-10-14 11:04:43 -0400

    Fix regex typo that let a nitro scam url through.

--- src/auto_reply/auto_reply.rs
+++ src/auto_reply/auto_reply.rs
@@ -78,8 +78,8 @@ static NITRO_SCAM_HAS_LINK: Lazy<RwLock<Regex>> = Lazy::new(|| RwLock::new(creat
 ], true)));
 
 static NITRO_SCAM_IGNORE: Lazy<RwLock<Regex>> = Lazy::new(|| RwLock::new(create_auto_reply_regex(&[
-    format!(r"https?:\/\/(?:www\.)?discord.gift"),
-    format!(r"https?:\/\/(?:www\.)?tenor.com")
+    format!(r"https?:\/\/(?:www\.)?discord\.gift"),
+    format!(r"https?:\/\/(?:www\.)?tenor\.com")
 ], true)));
 
 struct Info<'a> {
