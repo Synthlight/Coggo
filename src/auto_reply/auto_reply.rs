400f2c4e41719708ac8247692e326b3f27891961
Gregory Brutsche <gregory@mail.brutsches.local>
2022-01-15 21:08:01 -0500

    Cover a new discord misspelling.

--- src/auto_reply/auto_reply.rs
+++ src/auto_reply/auto_reply.rs
@@ -73,7 +73,7 @@ static NITRO_SCAM: Lazy<RwLock<Regex>> = Lazy::new(|| RwLock::new(create_auto_re
     format!(r"(?:(?:free.*?nitro)|(?:nitro.*?free))"),
     format!(r"(?:(?:steam.*?nitro)|(?:nitro.*?steam))"),
     format!(r"(?:(?:gift.*?nitro)|(?:nitro.*?gift))"),
-    format!(r"(?!discord\.)d[il]{{1,2}}[szck]{{1,3}}o(?:cl|r[dcl]{{1,2}}|[crd]{{1,3}})n?(?:n[il]tro)?"),
+    format!(r"(?!discord\.)d[il]{{1,2}}[szckr]{{1,3}}o(?:cl|r[dcle]{{1,2}}|[crd]{{1,3}})n?(?:n[il]tro)?"),
     format!(r"tinyurl\.com\/y2bffk8j"),
     format!(r"take nitro")
 ], true)));
